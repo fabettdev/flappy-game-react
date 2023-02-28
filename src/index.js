fa25c3260cfb6acd057ddc74e31146986746af7b
giuvino <giova.testa99@gmail.com>
2023-02-28 09:05:45 +0100

    parallasse

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/common.css';
+import './assets/styles/common.css';
 import EntryApp from './screens/EntryApp';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
