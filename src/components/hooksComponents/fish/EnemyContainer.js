2f0708816e54630df41ad6a6312c7cd2c8cc7bc5
fabettdev <fabrizioettori@gmail.com>
2023-03-01 17:06:22 +0100

    added enemy spawn

--- src/components/hooksComponents/fish/EnemyContainer.js
+++ src/components/hooksComponents/fish/EnemyContainer.js
@@ -1,33 +1,63 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './fish.css'
 import FishAutentico from './FishAutentico'
 import './enemycontainer.css'
 
+// Massimo 9 pesci
+// Stato pesci sopra
+// Numero random da 1 a 9
+// Stato pesci sopra diventa il numero che esce
+// Stato pesci sotto diventa numero casuale tra 0 e 9 - pesci sopra)
+
 function EnemyContainer() {
+
+    const [state, setState] = useState({
+        maxFishes: 9,
+        upperFishes: [],
+        lowerFishes: [],
+        translateX: -100,
+    })
+
+    useEffect(() => {
+        const upperFishes = [];
+        const lowerFishes = [];
+        const upperNum = Math.floor(Math.random() * 9 + 1);
+        const lowerNum = Math.floor(Math.random() * (state.maxFishes - upperNum + 1));
+        console.log(upperNum, lowerNum)
+        for (let i = 0; i < upperNum; i++) {
+            const fish = <div key={`${i}-${Math.random()}`} className='enemy-container'><FishAutentico /></div>;
+            upperFishes.push(fish);
+        }
+        for (let i = 0; i < lowerNum; i++) {
+            const fish = <div key={`${i}-${Math.random()}`} className='enemy-container'><FishAutentico /></div>;
+            lowerFishes.push(fish);
+        }
+        setState({
+            ...state,
+            upperFishes,
+            lowerFishes
+        })
+    }, [])
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setState(
+                {
+                    ...state,
+                    translateX: state.translateX + 2,
+                }
+            )
+        }, 10)
+        return () => clearTimeout(timeout);
+    }, [state]);
+
     return (
-        <div className='pillars-container'>
+        <div className='pillars-container' style={{ right: `${state.translateX}px` }}>
             <div className='enemy-pillar-top'>
-                <div className='enemy-container'>
-                    <FishAutentico />
-                </div>
-                <div className='enemy-container'>
-                    <FishAutentico />
-                </div>
-                <div className='enemy-container'>
-                    <FishAutentico />
-                </div>
+                {state.upperFishes.map(fish => fish)}
             </div>
-            <div className='divider'></div>
             <div className='enemy-pillar-bottom'>
-                <div className='enemy-container'>
-                    <FishAutentico />
-                </div>
-                <div className='enemy-container'>
-                    <FishAutentico />
-                </div>
-                <div className='enemy-container'>
-                    <FishAutentico />
-                </div>
+                {state.lowerFishes.map(fish => fish)}
             </div>
         </div>
     )
